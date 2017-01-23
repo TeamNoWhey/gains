3b810f640402cb27ad84cf3c7a2be9784c53a3dc
Rachel <racheldepriest1@gmail.com>
2017-01-23 10:57:44 -0600

    npm start works, and server has started listening on port 8080

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 // server.js
-
 var express = require('express')
+
 var app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('MASA is listening on port 3000!')
+var port = process.env.PORT || 8080;
+console.log('port:', port)
+
+app.listen(port, function () {
+  console.log('MASA is listening on port:', port)
 })
