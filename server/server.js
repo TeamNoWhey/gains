558f78bb409e46d2ce982d5faa05152a399901f8
Rachel <racheldepriest1@gmail.com>
2017-01-23 11:13:53 -0600

    Error handling is working for GET

--- server/server.js
+++ server/server.js
@@ -3,9 +3,21 @@ var express = require('express')
 
 var app = express()
 
+//Gets us to landing page
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+
+//error handler
+app.use(function(err, res, req, next){
+	res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err 
+    });
+})
+
 
 var port = process.env.PORT || 8080;
 console.log('port:', port)
