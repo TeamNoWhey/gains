666ceabf353f50f57ed45a74c2ab2a8e34d4147b
Forrest Miller <forrest.w.miller@icloud.com>
2017-01-24 13:06:18 -0600

    completed HTML template for planner.html

---
+++ client/app/planner/planner.js
@@ -0,0 +1,10 @@
+angular.module('FILLMEIN.FILLMEIN', [])
+
+/*
+This will me our controller for our html templates for the
+/planworkout route
+*/
+
+.controller('PlannerController', function($scope){
+
+}
