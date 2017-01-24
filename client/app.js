0aa04e37ec9c512ba76af29cda4be8986b1a6425
Tin <tinytheyfon8@gmail.com>
2017-01-24 13:45:05 -0600

    Changed to routing and qotd.html

--- client/app.js
+++ client/app.js
@@ -1,3 +1,5 @@
+//Not in use yet. Will be used for routing later. Currently using static routing with express
+
 var app = angular.module ('FitnessApp', ['ngRoute']);
 
 app.config(function($routeProvider)  {
