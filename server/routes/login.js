9a57280573234f71c1d3db19717037f6dfa78599
Rachel <racheldepriest1@gmail.com>
2017-01-24 13:06:13 -0600

    Pushing everything I got

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,8 +1,16 @@
+console.log('This is login')
 var express = require('express');
+var path= require('path');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-     //Do whatever...
+router.get('/login', function(req, res, next) {
+  console.log('login');
+  // res.sendFile(path.join(__dirname, '../public/app.html'));
 });
+
+router.get('/logout', function(req, res, next) {
+	console.log('logout')
+	// res.redirect('/');
+})
 
 module.exports = router;
