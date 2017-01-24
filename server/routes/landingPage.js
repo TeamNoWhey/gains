a142fb3eaa2798d48f9a1059c7b606da25f2d5fc
Rachel <racheldepriest1@gmail.com>
2017-01-23 18:16:52 -0600

    Changed files for routing. Basic routing almost functional

--- server/routes/landingPage.js
+++ server/routes/landingPage.js
@@ -1,1 +1,8 @@
-landingPage.js 
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+     //Do whatever...
+});
+
+module.exports = router;
