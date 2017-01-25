2468c4b948e8b8c8d2c291cd113b591c126ac46c
Forrest Miller <forrest.w.miller@icloud.com>
2017-01-25 15:05:26 -0600

    added autocomplete controller, needs iteration on cleaning it up and changing variable names over in autocomplete.js, the js was imported from angular materials and featurs the variable names of an autocomplete state searcher

---
+++ client/app/planner/listofworkouttypes.js
@@ -0,0 +1,249 @@
+// Unsure how to export this client side
+
+module.export = '\
+AX Biceps (Incline, Sitting, Reverse Incline, Band Burnout),\
+E-Z Skullcrushers (Deep Flex, 90 Degree Ext.),\
+ROTC Ab Drill* (X Situps, X Crunches, Y PUs, X Toe Touches, X Jack Knives, Y PUs, Plank, Scissors, Y PUs),\
+Short Lunge* (Shorter Steps, Works Quads),\
+21 BB Curl,\
+21 Crunch* (MB Optional),\
+21 DB Curls,\
+Alternating Calf Raise* (3x20/40),\
+Alternating DB Curls,\
+Alternating DB Shrug,\
+Arnold Press**,\
+Back Extension*,\
+Band Bicep Curls* (Burnout),\
+Band Knee Lockout**,\
+Band Twist,\
+BB Bench Press Negatives (Inc. wt.),\
+BB Bench Press**,\
+BB Bent-Over Row**,\
+BB Clean and Press*,\
+BB Deadlift to Row,\
+BB Incline Bench Press,\
+BB Lunges*,\
+BB RDL*,\
+BB Shrug**,\
+BB Upright Row,\
+Bench Dip*,\
+Bent Over Waist Twists,\
+Bicep BB Curl,\
+Bicycles,\
+Bridge w/ Ball Squeeze,\
+Cable Crossover*,\
+Cable Crunch,\
+Cable Curl,\
+Cable Flye,\
+Cable Lower Pec Press,\
+Cable Overhead Tricep Pushaway,\
+Cable Reverse Curl,\
+Cable Rotational Chest Press,\
+Cable Rotational Row,\
+Cable Woodchopper Pushdowns,\
+Calf Raise* (3x20/40),\
+Canoes*,\
+Chest Dips*,\
+Clapping Plyo Push Ups (Burnout),\
+Close-Grip BB Bench*,\
+Close-Grip Chin Up,\
+Close-Grip DB Bench,\
+Close-Grip Smith Maching Bench* (Incr. wt.),\
+Concentration DB Curl,\
+Crossover Step-ups*,\
+DB L Raise**,\
+DB 3-way Lunge*,\
+DB 3-way Single Leg RDL*,\
+DB Alternating Chest Press*,\
+DB Alternating Incline Chest Press*,\
+DB Bench Press*,\
+DB Bent-Over Reverse Fly,\
+DB Bent-over Row*,\
+DB Bent-over Row*,\
+DB Chest Press,\
+DB Clean and Press,\
+DB Clean and Press,\
+DB Close-Grip Front Raise,\
+DB Cuban Press**,\
+DB Curl and Press,\
+DB Curls*,\
+DB Deadlifts,\
+DB Decline Press,\
+DB Drop Squat,\
+DB Farmer Carry,\
+DB Fielder Curls,\
+DB Flyes,\
+DB Front Raise**,\
+DB Holds,\
+DB Incline Front Raise*,\
+DB Kickback*,\
+DB Lateral Lunge*,\
+DB Lateral Lunge,\
+DB Lateral Raise with Rotation,\
+DB Lateral Raise**,\
+DB Leaning Lateral Raise,\
+DB Lunge*,\
+DB Neutral-Grip Bench Press,\
+DB Overload/Cheat Lateral Raise,\
+DB Pec Deck,\
+DB Physioball Alternating Press,\
+DB Pitchers Lunge and Reach,\
+DB Pullover,\
+DB Rear Deltid Fly,\
+DB Renegade Row*,\
+DB Reverse Lunge Curl,\
+DB Rotational Shoulder Press,\
+DB Row*,\
+DB Seated Alternating Front Raise*,\
+DB Seated Lateral Raise*,\
+DB Seated Shoulder Press**,\
+DB Shrugs*,\
+DB Side Step Ups*,\
+DB Single Arm Incline BP,\
+DB Single Leg RDL*,\
+DB Single-Arm Overhead Press*,\
+DB Skullcrushers*,\
+DB Split Squat*,\
+DB Standing Rows,\
+DB Standing Shoulder Press,\
+DB Static-Hold Front Raise,\
+DB Step-up*,\
+DB Thrusters,\
+DB Tripod Row,\
+DB Upright Row*,\
+DB Wall Squat w/ Physioball Support and MB Squeeze*,\
+DB Wall Squat w/ Physioball Support,\
+Deadlift**,\
+Decline Bench Crunch with Twist,\
+Decline Bench Crunch,\
+Decline Bench Press**,\
+Decline Cable Chest Press*,\
+Decline Pushup* (Burnout),\
+Diamond Push Ups*,\
+Drop-set DB Curl,\
+E-Z Preacher Curl,\
+E-Z Reverse Curl,\
+E-Z Standing French Press*,\
+Eccentric Step-aways,\
+Eccentric Step-Up (Step-Down),\
+Facepull,\
+Frog Kicks,\
+Front Pulldown*,\
+Front Squat**,\
+Full Leg Raise,\
+Goodmornings,\
+Gripper*,\
+Hammer DB Curl,\
+Hanging Knee Raise*,\
+Heels to Heaven*,\
+Hercules Curls*,\
+High Cable Fly,\
+Hip Abduction*,\
+Hip Adduction*,\
+Hip Bucks,\
+Hip Bucks,\
+Incline DB Bench Press,\
+Incline DB Curl*,\
+Incline DB Flye*,\
+Incline Push Ups (Burnout),\
+Inner Pec Cable Squeeze,\
+Inverted Row**,\
+Jack Knives*,\
+Kettle Ball Swings*,\
+Lat Pulldown Close-Grip,\
+Lat Pulldown*,\
+Lateral Plyo Pushups (Burnout),\
+Leg Curl*,\
+Leg Extension*,\
+Leg Press**,\
+Leg Raises,\
+Low Cable Chest Raise,\
+Machine Chest Press,\
+Machine Fly,\
+Machine Lat Pulldown,\
+Machine Row,\
+Machine Shoulder Press,\
+Machine Torso Rotation*,\
+MB Pass*,\
+MB Single Leg Squat w/ Rotation*,\
+Military Press**,\
+OBB Twisting Thrusters,\
+Overhead Cable Curl,\
+Overhead Cable Ext.,\
+Overhead DB Ext.,\
+Pec Deck*,\
+Physioball Bridge into Curl,\
+Physioball Hamstring Curls,\
+Physioball Reverse Hyperextension ,\
+Plank March,\
+Plank Opposite Knee to Elbow,\
+Plank Same Side Knee to Elbow,\
+Plank*,\
+Plate 8s*,\
+Plate Finger Curls,\
+Plate Front Raise to Ceiling* (Burnout),\
+Plate Front Raise** (Burnout),\
+Plate Pinch,\
+Plate Press,\
+Plate Squeeze,\
+Plate-Loaded Row**,\
+Preacher DB Curl,\
+Pronated DB Hammer Curl (Palms Facing Upward),\
+Pullover*,\
+Pullups*,\
+Push Press,\
+PVC Pipe Rotation (Both Ways),\
+Rear Delt Fly**,\
+Rear Delt Lateral Raise *,\
+Reverse BB Curl,\
+Reverse Cable Curl,\
+Reverse Crunch,\
+Reverse Grip Bench Press*,\
+Reverse Incline DB Curl,\
+Reverse Pec Deck,\
+Reverse-Grip Pushdown,\
+Rollups,\
+Rope Pushdown*,\
+Rotation DB Curl*,\
+Rotational High Row,\
+Russian Twist w/ MB*,\
+Seated Bent-over Lateral Raise,\
+Seated Cable Row,\
+Seated DB Curl*,\
+Seated DB Long Leg Should Press,\
+Side Raises*,\
+Single Arm High Cable Row,\
+Single Leg Squat w/ Jumping (No wt.),\
+Single Leg Squat w/ MB,\
+Single-Arm DB Overhead Ext.,\
+Single-Arm Overhead Cable Ext.,\
+Sit-up with Twist,\
+Smith Machine Crunch*,\
+Smith-Maching Single Leg Squat*,\
+Smith-Maching Single Leg Squat,\
+Split Squat Jumps,\
+Squat**,\
+Standing BB Press Behind Neck,\
+Standing Bent-over Lateral Raise,\
+Standing DB Row to Kickback,\
+Standing Waist Twist,\
+Starfish Crunches*,\
+Sumo Squat*,\
+T-Bar Row,\
+Tricep Dips,\
+Tubing External Rotation,\
+Tubing Rotational Row,\
+Tubing Squat to Standing Row,\
+Tubing Squat to Standing Row,\
+Tubing Sword Raises,\
+Upright DB Row,\
+V Situps,\
+Vs w/ MB,\
+V-Bar Pushdown*,\
+V-up Russian Twists w/ MB*,\
+Waist Twist,\
+Wall Sit w/ MB Squeeze*,\
+Weighted Dips,\
+Wide-grip BB Row,\
+Wide-grip Chin-ups,\
+Windshield Wipers*';
