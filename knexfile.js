99765bd878ac518c0cf5ed36068cc3ca51e17c0e
BLuEScioN <barnettn26@gmail.com>
2017-01-25 16:28:27 -0600

    Finally connected to psql db and am able to see masa-db

---
+++ knexfile.js
@@ -0,0 +1,59 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'masa-db'
+    }
+  }
+  // development: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     '',
+  //     password: ''
+  //   },
+  //   pool: {
+  //     min: 1,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
---
+++ migrations/20170124152255_setup.js
@@ -0,0 +1,35 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('exercises', function (table) {
+      table.increments(); // `id` int unsigned not null auto_increment primary key
+      table.string('name', 255);
+      table.string('muscle-group', 255);
+      table.timestamps();
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('exercises')
+  ])
+};
+
+// exports.up = function(knex, Promise) {
+//   return Promise.all([
+//     knex.schema.createTable('users', function (table) {
+//       table.increments(); // `id` int unsigned not null auto_increment primary key // user.increments('id').primary();
+//       table.string('name', 255);
+//       table.string('username', 255).unique();
+//       table.string('email', 255); // input validation should occur before indertion into the db
+//       table.string('password', 25);
+//       table.timestamps();
+//     });  
+//   ])
+// };
+
+// exports.down = function(knex, Promise) {
+//   return Promise.all([
+//     knex.schema.dropTable('users');
+//   ])
+// };
