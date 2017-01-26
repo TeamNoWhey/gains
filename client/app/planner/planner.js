9ebceb49fb78399d49a2351e3a47a463ca82f2dd
BLuEScioN <barnettn26@gmail.com>
2017-01-26 15:43:40 -0600

    working on planworkout html

--- client/app/planner/planner.js
+++ client/app/planner/planner.js
@@ -7,4 +7,19 @@ This will me our controller for our html templates for the
 
 .controller('PlannerController', function($scope){
 
+  // $scope.exercises = [];
+  $scope.exercisesExample = [
+    {name: 'incline bench', sets: 3, reps: 12, target_weight: 185},
+    {name: 'deadlift', sets: 4, reps: 8, target_weight: 225}
+    {name: 'db thrusters', sets: 3, reps: 12, target_weight: 30}
+  ];
+
+  $scope.addExercise = function(passInFormInputs) {
+    $scope.exercises.push({'id':'choice'+newItemNo});
+  };
+
+
+
+
+
 });
