b2098b7bcef22d31e2cf6c05737ac27b9f746dc5
BLuEScioN <barnettn26@gmail.com>
2017-01-27 10:15:50 -0600

    still working on planning workout html and general structure of app

--- client/app/planner/planner.js
+++ client/app/planner/planner.js
@@ -10,7 +10,7 @@ This will me our controller for our html templates for the
   // $scope.exercises = [];
   $scope.exercisesExample = [
     {name: 'incline bench', sets: 3, reps: 12, target_weight: 185},
-    {name: 'deadlift', sets: 4, reps: 8, target_weight: 225}
+    {name: 'deadlift', sets: 4, reps: 8, target_weight: 225},
     {name: 'db thrusters', sets: 3, reps: 12, target_weight: 30}
   ];
 
