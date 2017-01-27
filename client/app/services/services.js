6f35ab6168b2e3d5d19a8ed03b34638e5cb8e4de
BLuEScioN <barnettn26@gmail.com>
2017-01-27 15:50:54 -0600

    started data flow from client to server to db

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,38 +1,54 @@
-angular.module('shortly.services', [])
+angular.module('masa.services', [])
 
-.factory('Links', function ($http) { // throw into workout planner controller
-re
+.factory('WorkoutsFac', function ($http) { // throw into workout planner controller
+
   // need a post of all the exercises data to the server for storage into the db
   // (stretch) will need to make a GET request to the server for fetching of target weight from the db
   // (stretch) will need to make a GET request to the server in order to fetch a list of all the exercises for autocomplete
   //    (probably want to cache this)
 
 
-
-  var getAll = function() { 
-    return $http({
-      method: 'GET',
-      url: '/api/links'
-    })
-    .then(function (resp) {
-      return resp.data;
-    });
-  };
-
-  var addOne = function(url) {
+  var storeWorkout = function(exercisesData) {
     return $http({
       method: 'POST',
-      url: '/api/links',
-      data: url,
+      url: '/workoutHistory',
+      data: exercisesData
     })
-    .then(function (resp) {
-      return resp;
-    });
+    .then(function(resp) {
+      console.log('Successfully posted the data server side where it can be stored in the user\'s db:', resp.data);
+      return resp.data;
+    })
+    .catch(function(err) {
+      console.error(err);
+    })
   };
 
+
+  // var getAll = function() { 
+  //   return $http({
+  //     method: 'GET',
+  //     url: '/api/links'
+  //   })
+  //   .then(function (resp) {
+  //     return resp.data;
+  //   });
+  // };
+
+  // var addOne = function(url) {
+  //   return $http({
+  //     method: 'POST',
+  //     url: '/api/links',
+  //     data: url,
+  //   })
+  //   .then(function (resp) {
+  //     return resp;
+  //   });
+  // };
+
   return {
-    getAll: getAll,
-    addOne: addOne
+    storeWorkout: storeWorkout
+    // getAll: getAll,
+    // addOne: addOne
   };
 
   // var changeViewShorten = function() {
@@ -40,51 +56,51 @@ re
   //     $location.path(view);
   //   };
   // };
-})
-.factory('Auth', function ($http, $location, $window) {
-  // Don't touch this Auth service!!!
-  // it is responsible for authenticating our user
-  // by exchanging the user's username and password
-  // for a JWT from the server
-  // that JWT is then stored in localStorage as 'com.shortly'
-  // after you signin/signup open devtools, click resources,
-  // then localStorage and you'll see your token from the server
-  var signin = function (user) {
-    return $http({
-      method: 'POST',
-      url: '/api/users/signin',
-      data: user
-    })
-    .then(function (resp) {
-      return resp.data.token;
-    });
-  };
-
-  var signup = function (user) {
-    return $http({
-      method: 'POST',
-      url: '/api/users/signup',
-      data: user
-    })
-    .then(function (resp) {
-      return resp.data.token;
-    });
-  };
-
-  var isAuth = function () {
-    return !!$window.localStorage.getItem('com.shortly');
-  };
-
-  var signout = function () {
-    $window.localStorage.removeItem('com.shortly');
-    $location.path('/signin');
-  };
-
-
-  return {
-    signin: signin,
-    signup: signup,
-    isAuth: isAuth,
-    signout: signout
-  };
 });
+// .factory('Auth', function ($http, $location, $window) {
+//   // Don't touch this Auth service!!!
+//   // it is responsible for authenticating our user
+//   // by exchanging the user's username and password
+//   // for a JWT from the server
+//   // that JWT is then stored in localStorage as 'com.shortly'
+//   // after you signin/signup open devtools, click resources,
+//   // then localStorage and you'll see your token from the server
+//   var signin = function (user) {
+//     return $http({
+//       method: 'POST',
+//       url: '/api/users/signin',
+//       data: user
+//     })
+//     .then(function (resp) {
+//       return resp.data.token;
+//     });
+//   };
+
+//   var signup = function (user) {
+//     return $http({
+//       method: 'POST',
+//       url: '/api/users/signup',
+//       data: user
+//     })
+//     .then(function (resp) {
+//       return resp.data.token;
+//     });
+//   };
+
+//   var isAuth = function () {
+//     return !!$window.localStorage.getItem('com.shortly');
+//   };
+
+//   var signout = function () {
+//     $window.localStorage.removeItem('com.shortly');
+//     $location.path('/signin');
+//   };
+
+
+//   return {
+//     signin: signin,
+//     signup: signup,
+//     isAuth: isAuth,
+//     signout: signout
+//   };
+// });
