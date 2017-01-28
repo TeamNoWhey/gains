f95f5cd18f3c050d4f57913df422b9401975774f
Tin <tinytheyfon8@gmail.com>
2017-01-28 00:47:55 -0600

    Completed API get for quote of the day. Also sets a new background each day from API source.

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,6 +1,8 @@
 var app = angular.module('Home', []);
 
-app.controller('HomeCtrl', ['$scope', function($scope) {
+app.controller('HomeCtrl', function($scope, $http) {
+  var apiUrl = "http://quotes.rest/qod.json?category=inspire";
+
 
   $scope.goals = [
   'Goal #1 with a checkboxxxxx after',
@@ -9,4 +11,40 @@ app.controller('HomeCtrl', ['$scope', function($scope) {
   ];
   $scope.message = 'Everyone see this please';
 
-}]);
+
+  $scope.getQuote = function () {
+    $http.get(apiUrl)
+    .then(function(response) {
+      var data = response.data.contents.quotes;
+      data.forEach(function(item) {
+        console.log(item);
+        $scope.quote = item
+      });
+    });
+  }
+
+  // $scope.quote = QuoteCall.quote;
+
+});
+
+
+// app.factory = ('QuoteCall', ['$http', function($http) {
+//   var quote = 'Some quote';
+//   var apiUrl = "http://quotes.rest/qod.json?category=inspire";
+
+//   // $http({
+//   //   method: "GET",
+//   //   url: "http://quotes.rest/qod.json?category=inspire"
+//   // }).then(function success(response) {
+//   //   console.log('Success data: ', response.data);
+//   //   quote = response.data;
+//   // }, function error(response) {
+//   //   console.log('Error data: ', response.statusText);
+
+//   //   quote = response.statusText;
+//   // });
+// var getQuote = function () {
+//   return $http.get(apiUrl);
+// };
+//   return quote;
+// });
--- client/app/home/qotd.js
+++ client/app/home/qotd.js
@@ -2,4 +2,15 @@
 
 var app = angular.module('Home', []);
 
+app.service = ('QuoteCall', function($scope, $http) {
+  $http({
+    method: "GET",
+    url: "http://quotes.rest/qod.json?category=inspire"
+  }).then(function success(response) {
+    $scope.quote = response.data;
+  }, function error(response) {
+    $scope.quote = response.statusText;
+  });
+});
+
 // app.factory('qotdCall', )
