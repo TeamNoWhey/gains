dd4d1d541967ff8cd96e8b4e9ad14a0817339152
Forrest Miller <forrest.w.miller@icloud.com>
2017-01-31 10:23:54 -0600

    whoops

--- client/app/history/history.js
+++ client/app/history/history.js
@@ -1,5 +1,6 @@
 var app = angular.module('History', ['masa.services']);
 
+app.controller('HistoryCtrl', ['$scope', 'WorkoutsFac', '$window', function($scope, WorkoutsFac, $window) {
   // fake. comment out later
   $scope.seeWorkoutHistory = function() { // need to get all user's exercise/workout data
     console.log('trying to see if we can get user exercise and workout data back from db');
